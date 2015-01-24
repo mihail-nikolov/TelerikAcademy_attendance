3d8c84ab3790a2f4785a290f9c5e7ce70f6346d8
Mihail Nikolov <mihail.y.nikolov@abv.bg>
2015-01-24 19:56:34 +0200

    operators

--- C# 1/operators_expressions/BitWise/BitWise.cs
+++ C# 1/operators_expressions/BitWise/BitWise.cs
@@ -7,6 +7,14 @@ namespace BitWise
     {
         static void Main()
         {
+            int number = 5;
+            string numBin = Convert.ToString(number, 2).PadLeft(16, '0');
+            Console.WriteLine("{0} --> {1}", number, numBin);
+            int position = 3;
+            int mask = 1 << position;
+            int numbAndMask = number & mask;
+            int bit = numbAndMask >> position;
+            Console.WriteLine("bit at pos. {0} is {1}", position, bit);
         }
     }
 }
--- C# 1/operators_expressions/bit_from_int/BitExctraction.cs
+++ C# 1/operators_expressions/bit_from_int/BitExctraction.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace bit_from_int
 {
     class BitExctraction
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int number = 62241;
+            string numBin = Convert.ToString(number, 2).PadLeft(16, '0');
+            Console.WriteLine("{0} --> {1}", number, numBin);
+            int position = 11;
+            char theBit = numBin[numBin.Length - position - 1];
+            Console.WriteLine("bit at pos. {0} is {1}", position, theBit);
         }
     }
 }
