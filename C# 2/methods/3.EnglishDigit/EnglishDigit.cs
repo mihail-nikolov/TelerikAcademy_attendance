7a5ae43ea6118229c3027e3ecc233c01cb5d857f
Mihail Nikolov <mihail.y.nikolov@abv.bg>
2015-02-18 21:14:04 +0200

    methods

--- C# 2/methods/3.EnglishDigit/EnglishDigit.cs
+++ C# 2/methods/3.EnglishDigit/EnglishDigit.cs
@@ -11,8 +11,24 @@ namespace _3.EnglishDigit
 {
     class EnglishDigit
     {
+        static string GetEnglishDigit(int num)
+        {
+            Dictionary<char, string> digitMap = new Dictionary<char, string>()
+            {
+                {'0', "zero"}, {'1', "one"}, {'2', "two"}, {'3', "three"}, {'4', "four"}, {'5', "five"},
+                {'6' , "six"}, {'7', "seven"}, {'8' , "eight"}, {'9', "nine"}
+            };
+            string numStr = num.ToString();
+            char theLastDigit = numStr[numStr.Length - 1];
+            string answer = digitMap[theLastDigit];
+            return answer;
+        }
         static void Main()
         {
+            Console.Write("enter n = ");
+            int num = int.Parse(Console.ReadLine());
+            string answer = GetEnglishDigit(num);
+            Console.WriteLine("the last digit is {0}", answer);
         }
     }
 }
--- C# 2/methods/6.FirstLargerThanNeighbours/FirstLargerThanNeighbours.cs
+++ C# 2/methods/6.FirstLargerThanNeighbours/FirstLargerThanNeighbours.cs
@@ -12,8 +12,37 @@ namespace _6.FirstLargerThanNeighbours
 {
     class FirstLargerThanNeighbours
     {
+        static bool LargerNeighbours(int[] arr, int pos)
+        {
+            if (pos <= 0 || pos >= arr.Length - 1)
+            {
+                return false;
+            }
+            if (arr[pos] > arr[pos + 1] && arr[pos] > arr[pos - 1])
+            {
+                return true;
+            }
+            return false;
+        }
+        static int FirtLagrgerNeighbours(int[] arr)
+        {
+            int answer = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (LargerNeighbours(arr, i))
+                {
+                    answer = i;
+                    return answer;
+                }
+            }
+            return answer;
+        }
         static void Main()
         {
+            int[] arr = { 2, 10, 4, 5, 6, 8, 6, 2 };
+            int answer = FirtLagrgerNeighbours(arr);
+            Console.WriteLine(string.Join(", ", arr));
+            Console.WriteLine("answer: {0}", answer);
         }
     }
 }
