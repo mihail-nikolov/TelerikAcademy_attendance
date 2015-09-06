a029b7027f690ba5377941153dd86a0d05749857
Mihail Nikolov <mihail.y.nikolov@abv.bg>
2015-09-06 14:23:21 +0300

    asda

---
+++ KPP/homework/17.creational_patterns/Creational_patterns/Singleton_project/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton_project
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main()
+        {
+            Sun theSun = Sun.SunInstance;
+            theSun.Shine();
+        }
+    }
+}
