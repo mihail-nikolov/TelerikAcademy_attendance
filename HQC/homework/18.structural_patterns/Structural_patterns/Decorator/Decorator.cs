e73a184ae7396a0e074e7f4a2f671ecb101512d0
Mihail Nikolov <mihail.y.nikolov@abv.bg>
2015-09-17 21:50:45 +0300

    structural patterns

---
+++ HQC/homework/18.structural_patterns/Structural_patterns/Decorator/Decorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPatternExample
+{
+    /// <summary>
+    /// The 'Decorator' abstract class
+    /// </summary>
+    internal abstract class Decorator : Vehicle
+    {
+        protected Vehicle Vehicle { get; private set; }
+        protected Decorator(Vehicle vehicle)
+        {
+            this.Vehicle = vehicle;
+        }
+
+        public override void Display()
+        {
+            this.Vehicle.Display();
+        } 
+    }
+}
---
+++ HQC/homework/18.structural_patterns/Structural_patterns/Facade/ServeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePatternExample
+{
+    public class ServeController
+    {
+        public void ServeMeal()
+        {
+            Console.WriteLine("Serving the muesli...Bon apetit!");
+        }
+    }
+}
